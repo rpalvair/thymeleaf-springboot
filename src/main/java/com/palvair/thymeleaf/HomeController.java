d4be0b3e0ffea635ddaa4e75e3d6433f9d668cce
widdy <r.palvair@gmail.com>
2016-07-06 13:24:58 +0200

    Initial commit : fork from https://github.com/rpalvair/spring-boot/tree/master/springboot-springmvc

---
+++ src/main/java/com/palvair/thymeleaf/HomeController.java
@@ -0,0 +1,15 @@
+package com.palvair.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("msg", "Welcome to Spring boot mvc");
+        return "index";
+    }
+}
